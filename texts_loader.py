a03caf5841ca2dd0fca0e6f106175ea451c3e9e5
3Davydov <davydovdaniil501@gmail.com>
2024-05-01 13:38:31 +0700

    add character name removing

--- texts_loader.py
+++ texts_loader.py
@@ -1,5 +1,14 @@
 import os
+import re
 
+def remove_character_names(text):
+    # Шаблон регулярного выражения для поиска фамилий персонажей
+    pattern = r'(?<!\.)\b[А-Я][а-я]+\b'
+
+    # Удаление фамилий персонажей из текста
+    result = re.sub(pattern, '', text)
+
+    return result
 
 def get_texts_from_dir(files_dir):
 
@@ -10,5 +19,6 @@ def get_texts_from_dir(files_dir):
         file_path = os.path.join(files_dir, file_name)
         with open(file_path, 'r', encoding='utf-8') as file:
             text = file.read()
+            text = remove_character_names(text)
             test_texts.append(text)
     return test_texts
